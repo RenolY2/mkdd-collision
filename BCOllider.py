635c90387067fa33ba1b7892cc94c5bf6e828519
Yoshi2 <debugyoshi@gmail.com>
2017-07-28 11:52:29 +0200

    offset parsing

--- BCOllider.py
+++ BCOllider.py
@@ -3,10 +3,13 @@
 from struct import unpack_from
 
 def read_array(buffer, offset, length):
-
+    return buffer[offset:offset+length]
 
 def read_float(buffer, offset):
     return unpack_from(">I", buffer, offset)[0]
+
+def read_int32(buffer, offset):
+    return unpack_from(">i", buffer, offset)[0]
 
 def read_uint32(buffer, offset):
     return unpack_from(">I", buffer, offset)[0]
@@ -19,7 +22,8 @@ def read_uint8(buffer, offset):
 
 class RacetrackCollision(object):
     def __init__(self):
-        self.header = b"0003"
+        self._data = None
+        self.identifier = b"0003"
         self.grid_xsize = 0
         self.grid_zsize = 0
         self.coordinate1_x = 0
@@ -33,6 +37,7 @@ class RacetrackCollision(object):
         self.gridsoffset = 0
         self.trianglesoffset = 0
         self.verticesoffset = 0
+        self.unknownoffset = 0
 
         self.grids = []
         self.triangles = []
@@ -40,8 +45,38 @@ class RacetrackCollision(object):
 
     def load_file(self, f):
         data = f.read()
+        self._data = data
         if data[:0x4] != b"0003":
             raise RuntimeError("Expected header start 0003, but got {0}. "
                                "Likely not MKDD collision!".format(data[:0x4]))
 
+        self.identifier = data[:0x4]
 
+
+        self.grid_xsize = read_uint16(data, 0x4)
+        self.grid_zsize = read_uint16(data, 0x6)
+
+        self.coordinate1_x = read_int32(data, 0x8)
+        self.coordinate1_z = read_int32(data, 0xC)
+        self.coordinate2_x = read_int32(data, 0x10)
+        self.coordinate2_z = read_int32(data, 0x14)
+
+        self.entrycount = read_uint16(data, 0x18)
+        self.padding = read_uint16(data, 0x1A)
+
+        self.gridtableoffset = 0x2C
+        self.gridsoffset = read_uint32(data, 0x1C)
+        self.trianglesoffset = read_uint32(data, 0x20)
+        self.verticesoffset = read_uint32(data, 0x24)
+        self.unknownoffset = read_uint32(data, 0x28)
+
+
+
+if __name__ == "__main__":
+    collision = RacetrackCollision()
+    with open("mkddcol/babyluigi_course.bco", "rb") as f:
+        collision.load_file(f)
+
+    for i in (collision.gridsoffset, collision.trianglesoffset, collision.verticesoffset, collision.unknownoffset):
+        print(hex(i))
+    print(hex(len(collision._data)))
