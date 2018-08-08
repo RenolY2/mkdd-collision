4824846894da739b460602fea18792a8c6bd08a6
Yoshi2 <debugyoshi@gmail.com>
2018-08-08 21:13:50 +0200

    random uncommited stuff

--- BCOllider.py
+++ BCOllider.py
@@ -3,23 +3,33 @@
 import subprocess
 from struct import unpack_from, pack
 
+from BCOnvert import (normalize_vector, create_vector, cross_product, 
+                        write_float, write_uint32, write_short)
+
+
 def read_array(buffer, offset, length):
     return buffer[offset:offset+length]
+
 
 def read_float(buffer, offset):
     return unpack_from(">f", buffer, offset)[0]
 
+
 def read_int32(buffer, offset):
     return unpack_from(">i", buffer, offset)[0]
+
 
 def read_uint32(buffer, offset):
     return unpack_from(">I", buffer, offset)[0]
 
+
 def read_uint16(buffer, offset):
     return unpack_from(">H", buffer, offset)[0]
 
+
 def read_uint8(buffer, offset):
     return unpack_from("B", buffer, offset)[0]
+
 
 class RacetrackCollision(object):
     def __init__(self):
@@ -80,7 +90,6 @@ class RacetrackCollision(object):
             v2 = read_int32(data, self.trianglesoffset+i*0x24 + 0x04)
             v3 = read_int32(data, self.trianglesoffset+i*0x24 + 0x08)
             rest = read_array(data, self.trianglesoffset+i*0x24 + 0x0C, length=0x24-0xC)
-
             self.triangles.append((v1,v2,v3,rest))
 
         # Parse vertices
@@ -117,6 +126,7 @@ def read_gridtable_entry(data, offset):
 
     return unk1, unk2, gridtableindex, triangleindices_index
 
+
 def get_grid_entries(data, index, offset, limit, f, indent, gottem):
     unk1, unk2, nextindex, triangleindex_offset = read_gridtable_entry(data, offset)
     f.write("{0}index: {1}| {2} {3} {4} {5}\n".format(indent*4*" ", index, unk1, unk2, nextindex, triangleindex_offset))
@@ -128,6 +138,7 @@ def get_grid_entries(data, index, offset, limit, f, indent, gottem):
             assert offset < limit
             gottem[nextindex+i] = True
             get_grid_entries(data, nextindex+i, offset, limit, f, indent+1, gottem)
+
 
 if __name__ == "__main__":
     col = RacetrackCollision()
@@ -218,46 +229,89 @@ if __name__ == "__main__":
 
     assert 0x2C + total_gridentries*8 == col.triangles_indices_offset
 
-    with open("H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\luigi2\\luigi_course.bco", "wb") as f:
+    #with open("H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\luigi2\\luigi_course.bco", "wb") as f:
+    with open("F:/Wii games/MKDDModdedFolder/P-GM4E/files/Course/daisy/daisy_course.bco", "wb") as f:
         f.write(col._data[:col.trianglesoffset])
         replace = 0x18
         for v1, v2, v3, rest in col.triangles:
+            start = f.tell()
             f.write(pack(">III", v1, v2, v3))
             floatval = read_float(rest, 0x00)
 
             #f.write(pack(">f", floatval-200))
-            f.write(rest[:replace-0xC])
+            #f.write(rest[:replace-0xC])
             #f.write(rest[:0xA])
             #f.write(b"\x00"*2)
             #f.write(b"\x00"*10)
             #f.write(pack("B", 6))
-            normx, normy, normz = map(lambda x: x/10000.0, unpack_from(">hhh", rest, 0x4))
-            #print("---")
-            v1x, v1y, v1z = col.vertices[v1]
-            v2x, v2y, v2z = col.vertices[v2]
-            v3x, v3y, v3z = col.vertices[v3]
+            #normx, normy, normz = map(lambda x: x/10000.0, unpack_from(">hhh", rest, 0x4))
+            #v1x, v1y, v1z = col.vertices[v1]
+            #v2x, v2y, v2z = col.vertices[v2]
+            #v3x, v3y, v3z = col.vertices[v3]
+            v1vec = col.vertices[v1]
+            v2vec = col.vertices[v2]
+            v3vec = col.vertices[v3]
+            
+            v1tov2 = create_vector(v1vec,v2vec)
+            v2tov3 = create_vector(v2vec,v3vec)
+            v3tov1 = create_vector(v3vec,v1vec)
+            v1tov3 = create_vector(v1vec,v3vec)
+            
+            
+            cross_norm = cross_product(v1tov2, v1tov3)
+            
+            if cross_norm[0] == cross_norm[1] == cross_norm[2] == 0.0:
+                norm = cross_norm
+                print("norm calculation failed")
+            else:
+                norm = normalize_vector(cross_norm)
 
-
-            midx = (v1x+v2x+v3x)/3.0
-            midy = (v1y+v2y+v3y)/3.0
-            midz = (v1z+v2z+v3z)/3.0
+            normx = int(round(norm[0], 4) * 10000)
+            normy = int(round(norm[1], 4) * 10000)
+            normz = int(round(norm[2], 4) * 10000)
+            
+            #midx = (v1x+v2x+v3x)/3.0
+            #midy = (v1y+v2y+v3y)/3.0
+            #midz = (v1z+v2z+v3z)/3.0
+            
+            
             #print(normx, normy, normz)
-            #print(midx*normx+midy*normy+midz*normz)
+            ownval = (-1)*(v1vec[0]*norm[0]+v1vec[1]*norm[1]+v1vec[2]*norm[2])
+            #print(ownval, floatval)
+            if abs(floatval - ownval) > 4:
+                print(floatval, ownval)
 
             #print(floatval)
+            
+            write_float(f, ownval)
+            write_short(f, normx)
+            write_short(f, normy)
+            write_short(f, normz)
+            
             vertices = [col.vertices[v1], col.vertices[v2], col.vertices[v3]]
             val = rest[0x18-0xC] # Get byte at 0x18
             max_z, max_x, min_z, min_x= ((val>>6)&0b11, (val>>4)&0b11,(val>>2)&0b11, val&0b11)
             #print(min(v1x,v2x,v3x),     min(v1z,v2z,v3z), max(v1x,v2x,v3x), max(v1z,v2z,v3z))
             #print(vertices[min_x][0],   vertices[min_z][2], vertices[max_x][0], vertices[max_z][2])
 
-            f.write(pack("B", rest[0x18-0xC]))
+            #f.write(pack("B", rest[0x18-0xC]))
 
             #f.write(pack(">H", 0x00))
             #f.write(rest[0xA:0xC])
-            f.write(rest[(replace+1)-0xC:])
+            #f.write(rest[(replace+1)-0xC:])
 
+            #f.write(rest[:0x19-0x0C])
+            #f.write(rest[:0x16-0x0C])
+            f.write(pack(">H", 0x100))
+            #f.write(rest[:0x18-0x0C])
+            f.write(pack(">B", rest[0x18-0x0C]))
+            f.write(b"\x01")
+            f.write(b"\xFF"*6)
+            #f.write(rest[0x20-0x0C:])
+            f.write(b"\x00"*4)
+            assert f.tell() - start == 0x24
             #f.write(rest)
         f.write(col._data[col.verticesoffset:])
 
-    subprocess.call(["H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\ArcPack.exe", "H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\luigi2"])
+    #subprocess.call(["H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\ArcPack.exe", "H:\\Games\\Nintendo Modding\\MarioKartDD\\Course\\luigi2"])
+    subprocess.call(["F:/Wii games/MKDDModdedFolder/P-GM4E/files/Course/ArcPack.exe", "F:/Wii games/MKDDModdedFolder/P-GM4E/files/Course/daisy"])
